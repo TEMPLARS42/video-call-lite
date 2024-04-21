6fd48d3171c7f0af75e65efe6ce2ba9a49233bce
Templars42 <42905854+TEMPLARS42@users.noreply.github.com>
2024-04-21 11:53:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = 80;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
